3704d9802c8eed12d464a8e082f7c3c6323ed360
Fulya Çelebi <celebifulya@gmail.com>
2021-03-02 22:31:33 +0300

    added redux, and implemented userReducer and userActions in Header and App components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
@@ -32,4 +33,12 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
+
+// const mapStateToProps = (state) => ({
+//   currentUser: state.user.currentUser,
+// });
+
+export default connect(mapStateToProps)(Header);
