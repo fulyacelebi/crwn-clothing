6875d5eb275d1b50d525205f26b42ec15654faac
Fulya Çelebi <celebifulya@gmail.com>
2021-05-22 16:31:00 +0300

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with redux flow for directory/shop

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,5 +1,20 @@
+import memoize from 'lodash.memoize';
 import { createSelector } from 'reselect';
 
 const selectShop = (state) => state.shop;
 
-export const selectCollections = createSelector([selectShop], (shop) => shop.collections);
+export const selectCollections = 
+    createSelector(
+        [selectShop], 
+        (shop) => shop.collections);
+
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    colletions => Object.keys(colletions).map(key => colletions[key])
+)
+
+export const selectCollection = memoize(collectionUrlParam => 
+    createSelector (
+        [selectCollections],
+        collections => collections[collectionUrlParam]
+    ));
